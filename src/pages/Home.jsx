5f05e1f584ba587a4c231c34c393d30072933db1
Hilbert Ruban <hilbert.r@targetbay.com>
2024-05-20 00:57:50 +0530

    add island 3d mesh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
+import Island from '../models/Island'
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let islandScale = null;
+    let islandPosition = [0, -6.5, -43];
+    let rotation = [.1, 4.7, 0];
+    
+    islandScale = window.innerWidth < 768 ? [.9, .9, .9] : [1,1,1];
+
+    return [islandScale, islandPosition, rotation];
+  }
+
+  const [IslandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -10,7 +22,14 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          
+          <directionalLight position={[1,1,1]} intensity={2}/>
+          <ambientLight intensity={.5}/>
+          <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1}/>
+          <Island 
+            position={islandPosition}
+            scale={IslandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
