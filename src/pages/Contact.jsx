df736a6cc022e77aae1a2c9ee8fcaf2b981668d4
Hilbert Ruban <hilbertruban53@gmail.com>
2024-05-21 02:51:07 +0530

    implement mail functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,54 @@
+import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   const handleFocus = () => {};
   const handleBlur = () => {};
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+console.log(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,);
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Hilbert",
+          from_email: form.email,
+          to_email: "hilbertruban53@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        // TODO: Show success message
+        // TODO: Hide an alert
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+        // TODO: SHOW ERROR MESSAGE
+      });
+  };
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form 
+        <form
           className="w-full flex flex-col gap-7 mt-14"
           onSubmit={handleSubmit}
-          >
+        >
           <label className="text-black-500 font-semibold">
             Name
             <input
@@ -67,7 +98,7 @@ const Contact = () => {
             onFocus={handleFocus}
             onBlur={handleBlur}
           >
-            {isLoading ? 'Sending...' : 'Send Message'}
+            {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
       </div>
